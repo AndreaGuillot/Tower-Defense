0db0ee7b92b6a0868744d94522a6821be2184ae2
Margaux <margauxvllt@gmail.com>
2019-05-01 17:02:25 +0200

    ajout fonctions get + constructeur
    J'ai mis dans des struct comme Steeve du coup !

--- src/struct.h
+++ src/struct.h
@@ -4,80 +4,100 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct joueur{
+struct joueur{
 	int largent;
-}joueur;
+};
 
 //là pour détruire des monstres
-typedef struct towerDefense{
-    unsigned int energie;
+struct towerDefense{
 	unsigned int puissance; //dégats effectués par la tour
 	unsigned int portee; //en pixels, la distance à laquelle la tour peut tirer
 	unsigned int cadence; //en nb de dixieme de seconde l'intervalle entre deux tirs
 	unsigned int cout;
-}towerDefense;
 
-typedef struct towerDefenseRouge{ //puissance ++, cadence -
+	towerDefense(uint puissance, uint portee, uint cadence){
+		this->puissance = puissance;
+		this->portee = portee;
+		this->cadence = cadence;
+	}
+
+	uint getPuissance(){
+		return this->puissance;
+	}
+
+	uint getPortee(){
+		return this->portee;
+	}
+
+	uint getCadence(){
+		return this->cadence;
+	}
+
+	uint getCout(){
+		return this->cout;
+	}
+};
+
+struct towerDefenseRouge{ //puissance ++, cadence -
 	towerDefense();
-}towerDefenseRouge;
+};
 
-typedef struct towerDefenseVert{ //cadence ++, portee -, puissance ~
+struct towerDefenseVert{ //cadence ++, portee -, puissance ~
 	towerDefense();
-}towerDefenseVert;
+};
 
-typedef struct towerDefenseJaune{ //puissance -, portee --, cadence +
+struct towerDefenseJaune{ //puissance -, portee --, cadence +
 	towerDefense();
-}towerDefenseJaune;
+};
 
-typedef struct towerDefenseBleu{ //portee ++, cadence ++, puissance --
+struct towerDefenseBleu{ //portee ++, cadence ++, puissance --
 	towerDefense();
-}towerDefenseBleu;
+};
 
-/*Pour définir les types de tours, on va mettre dans le fichier .cpp :
-
-towerDefenseRouge::towerDefenseRouge()
-:   towerDefense { energie, puissance, portee, cadence}
-{
-}
-
-Regarder dans les TP de Steeve si c'est mieux de prendre class ou struct
-*/
-
-typedef struct monstres{
+struct monstres{
 	int pv;
 	int resistance;
 	int vitesse;
-}monstres;
+};
 
-typedef struct vagueMonstres{
+struct vagueMonstres{
 	int nbMonstre = 10;
-}vagueMonstres;
+};
 
-typedef struct installation{
+struct installation{
 	unsigned int height;
 	unsigned int width;
 	unsigned int portee;
 	unsigned int cout;
-}installation;
 
-typedef struct radar{
+	installation(uint portee, uint cout){
+		this->portee = portee;
+		this->cout = cout;
+	}
+
+	uint getPortee(){
+		return this->portee;
+	}
+
+	uint getPortee(){
+		return this->portee;
+	}
+};
+
+struct radar{
 	installation();
 	//Augmente de 25% la portée des tours autour
-}radar;
+};
 
-typedef struct usineArmement{
+struct usineArmement{
 	installation();
 	//Augmente de 25% la puissance des tours autour
-}usineArmement;
+};
 
-typedef struct stockMunition{
+struct stockMunition{
 	installation();
 	//Augmente de 25% la cadence des tours autour
-}stockMunition;
+};
 
-
-typedef struct cartes{
-
-}cartes;
 
 #endif
